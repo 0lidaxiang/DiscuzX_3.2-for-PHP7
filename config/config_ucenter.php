59eceba33d12f279b2df5f08317998f9c698ca63
mst8222 <mst8222@163.com>
2016-10-21 14:40:04 +0800

    config

---
+++ config/config_ucenter.php
@@ -0,0 +1,19 @@
+<?php
+
+
+define('UC_CONNECT', '');
+
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '85668029');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'Ydx234hi123raspi');
+define('UC_API', 'http://localhost:92');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
